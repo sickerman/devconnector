ea981fb72e0015902aa557a63dafd8ad7cb4e21a
sickermode <henryoke158@yahoo.com>
2020-04-23 23:08:12 -0400

    pushing to heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const connectDB = require("./config/db");
 
 const app = express();
@@ -18,6 +19,8 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+app.use(morgan("tiny"));
 
 const PORT = process.env.PORT || 5000;
 
