179f7632ac9e9419cbdf944d5faf8f497d48b027
sickermode <henryoke158@yahoo.com>
2020-04-25 07:36:50 -0400

    new commit

--- server.js
+++ server.js
@@ -26,4 +26,8 @@ app.use(morgan("tiny"));
 
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 app.listen(PORT, () => console.log(`Server listening at port ${PORT}...`));
